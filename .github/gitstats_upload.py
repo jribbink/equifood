5881665c7b31b76d1684febb76b8c2c1024823a9
Jordan Ribbink <jribbink@telus.net>
2022-10-07 22:52:17 -0700

    Fix timezone bug

--- .github/gitstats_upload.py
+++ .github/gitstats_upload.py
@@ -58,7 +58,7 @@ except:
 repository = "jribbink/equifood"
 group_name = "Equifood Group C" + (" " + week if week is not None else "")
 
-today = datetime.utcnow().astimezone(pytz.timezone('US/Pacific'))
+today = datetime.now().astimezone(pytz.timezone('US/Pacific'))
 last_saturday = (today - timedelta(((today.weekday() - 5) % 7))).replace(hour=0, minute=0, second=0, microsecond=0)
 next_friday = (today + timedelta((today.weekday() - 4) % 7)).replace(hour=23, minute=59, second=59, microsecond=999)
 
