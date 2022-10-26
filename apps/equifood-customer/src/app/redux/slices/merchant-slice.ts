5a038de1dae86566e804cac0860a52e09b8ab01d
Jordan Ribbink <jribbink@telus.net>
2022-10-26 13:38:55 -0700

    Add redux reducer for merchants api requests

---
+++ apps/equifood-customer/src/app/redux/slices/merchant-slice.ts
@@ -0,0 +1,49 @@
+import { Merchant } from '@equifood/api-interfaces';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getMerchants = createAsyncThunk(
+  'merchants/getMerchants',
+  async () => {
+    const response = await axios.get('/merchants');
+    return response.data;
+  }
+);
+
+interface MerchantState {
+  merchants: Merchant[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error?: string;
+}
+
+const initialState: MerchantState = {
+  merchants: [],
+  status: 'idle',
+};
+
+export const merchantSlice = createSlice({
+  name: 'merchants',
+  initialState,
+  reducers: {
+    addMerchants(state, action) {
+      state.merchants.push(action.payload);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(getMerchants.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    builder.addCase(getMerchants.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.merchants = [...action.payload];
+    });
+    builder.addCase(getMerchants.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+  },
+});
+
+export const { addMerchants } = merchantSlice.actions;
+
+export default merchantSlice.reducer;
--- apps/equifood-customer/src/app/screens/Home/Home.tsx
+++ apps/equifood-customer/src/app/screens/Home/Home.tsx
@@ -1,27 +1,23 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { VStack, ScrollView } from 'native-base';
 import MerchantCard from '../../components/cards/MerchantCard/MerchantCard';
 import { Merchant } from '@equifood/api-interfaces';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import { getMerchants } from '../../redux/slices/merchant-slice';
+import { AppDispatch, RootState } from '../../redux/store';
 
 const Home = () => {
-  const [merchants, setMerchants] = useState<Merchant[]>([
-    {
-      id: '1',
-      name: 'Test',
-      banner_url: 'https://example.com/foo.png',
-    },
-    {
-      id: '2',
-      name: 'Test',
-      banner_url: 'https://example.com/foo.png',
-    },
-    {
-      id: '3',
-      name: 'Test',
-      banner_url: 'https://example.com/foo.png',
-    },
-  ]);
+  const store = useStore<RootState>();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const merchants = useSelector<RootState, Merchant[]>(
+    () => store.getState().merchants.merchants
+  );
+
+  useEffect(() => {
+    dispatch(getMerchants());
+  }, [dispatch]);
 
   return (
     <ScrollView>
