3404d0b490c31e6dca5b51037533ab4570b00788
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-10-26 11:35:13 -0700

    Tab bar is working but might need a little bit of tweaking.

--- apps/equifood-customer/src/app/layout/DefaultLayout/DefaultLayout.tsx
+++ apps/equifood-customer/src/app/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,19 +1,42 @@
 import { Box, Text } from 'native-base';
 import { ReactNode } from 'react';
 import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 interface Props {
   children?: ReactNode;
 }
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </SafeAreaView>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function DefaultLayout({ children }: Props) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Box flex={1}>
         {children}
-        <Box bgColor="green.400">
-          <Text>This is where navigation needs to go...</Text>
-        </Box>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </Box>
     </SafeAreaView>
   );
