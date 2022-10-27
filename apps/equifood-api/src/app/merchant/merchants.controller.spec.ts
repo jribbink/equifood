78278b99cec90000fbcf33ac9acf19559cc6e5c7
Jordan Ribbink <jribbink@telus.net>
2022-10-26 23:13:26 -0700

    Add DB mocking with better-sqlite3 in memory

--- apps/equifood-api/src/app/merchant/merchants.controller.spec.ts
+++ apps/equifood-api/src/app/merchant/merchants.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Merchant } from './entities/merchant.entity';
 import { MerchantsController } from './merchants.controller';
+import { MerchantsService } from './merchants.service';
+import { TypeOrmSqlLiteTestingModule } from '../../../test-utils/typeorm-test.module';
 
 describe('MerchantsController', () => {
   let controller: MerchantsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmSqlLiteTestingModule(Merchant)],
       controllers: [MerchantsController],
+      providers: [MerchantsService],
     }).compile();
 
     controller = module.get<MerchantsController>(MerchantsController);
--- apps/equifood-api/src/app/merchant/merchants.service.spec.ts
+++ apps/equifood-api/src/app/merchant/merchants.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmSqlLiteTestingModule } from '../../../test-utils/typeorm-test.module';
+import { Merchant } from './entities/merchant.entity';
 import { MerchantsService } from './merchants.service';
 
 describe('MerchantService', () => {
@@ -6,6 +8,7 @@ describe('MerchantService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmSqlLiteTestingModule([Merchant])],
       providers: [MerchantsService],
     }).compile();
 
