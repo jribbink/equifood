2136d6328e67710d877171c22ca44051f8affa0a
jcoll059 <jordancolledge059@gmail.com>
2022-10-27 19:37:51 -0700

    Added text entry

--- apps/equifood-customer/src/app/screens/Login/Login.tsx
+++ apps/equifood-customer/src/app/screens/Login/Login.tsx
@@ -1,10 +1,16 @@
 import React, { useRef, useState } from 'react';
 
 import { VStack, ScrollView } from 'native-base';
+import { Button, Text, View, SafeAreaView, StyleSheet, TextInput } from "react-native";
 import MerchantCard from '../../components/cards/MerchantCard/MerchantCard';
 import { Merchant } from '@equifood/api-interfaces';
+import { authenticate} from '../../redux/slices/auth-slice';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import { AppDispatch, RootState } from '../../redux/store';
 
 const Login = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   const [merchants, setMerchants] = useState<Merchant[]>([
     {
       id: '1',
@@ -23,13 +29,47 @@ const Login = () => {
     },
   ]);
 
+  const [un, onChangeUsername] = useState("");
+  const [pw, onChangePassword] = useState("");
+
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+
   return (
     <ScrollView>
-      <VStack space={3} paddingX={2}>
+      <VStack space={3} paddingX={2} marginBottom={10}>
         {merchants.map((merchant) => (
           <MerchantCard merchant={merchant} key={merchant.id}></MerchantCard>
         ))}
       </VStack>
+      <SafeAreaView>
+        <Text style={{padding: 10, fontSize: 24}}>
+          Username
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeUsername}
+          value={un}
+        />
+        <Text style={{padding: 10, fontSize: 24}}>
+          Password
+        </Text>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={pw}
+        />
+      </SafeAreaView>
+
+      <Button title="Login" onPress={() => (dispatch(authenticate(un, pw)))} color="#841584" accessibilityLabel="Learn more about this purple button" />
+      
     </ScrollView>
   );
 };
