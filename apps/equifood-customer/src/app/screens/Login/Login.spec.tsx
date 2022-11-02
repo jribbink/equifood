83efb430540a25c085033213250ac8bae0594f55
jcoll059 <jordancolledge059@gmail.com>
2022-11-02 09:47:54 -0700

    basic test case created, needs workshopping

--- apps/equifood-customer/src/app/screens/Login/Login.spec.tsx
+++ apps/equifood-customer/src/app/screens/Login/Login.spec.tsx
@@ -3,7 +3,23 @@ import { render } from '../../../test-utils/render';
 
 import Login from './Login';
 
+import { authenticate } from '../../redux/slices/auth-slice';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import { AppDispatch, RootState } from '../../redux/store';
+
 test('renders correctly', () => {
   const { getByTestId } = render(<Login />);
   expect(getByTestId('login')).toHaveTextContent('Login');
+});
+
+test('connects to backend endpoint', () => {
+  // I don't know how to test the backend connection itself because I'm a fool
+  // but I'm going to test the dispatch(authenticate()) anyway
+
+  const store = useStore<RootState>();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const email = 'johndoe@teleworm.us';
+  const pw = 'pass';
+  dispatch(authenticate({ email, pw }));
 });
