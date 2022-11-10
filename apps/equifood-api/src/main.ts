9d0f08d1f7044e6e6e04b494ff99add23241ee08
Jordan Ribbink <jribbink@users.noreply.github.com>
2022-11-09 16:45:02 -0800

    Add @nestjs/swagger (#115)

--- apps/equifood-api/src/main.ts
+++ apps/equifood-api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Initialize swagger (API docs)
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Listen on HTTP port
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
