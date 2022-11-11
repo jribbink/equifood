2f894320f7b9e3c380ec7f11c008c47e7dcb0b5a
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-11-10 16:38:38 -0800

    The gap exists now, but idk how it would interact with apple.

--- apps/equifood-customer/src/app/layouts/CoreLayout/CoreLayout.tsx
+++ apps/equifood-customer/src/app/layouts/CoreLayout/CoreLayout.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Home from '../../screens/Home/Home';
 import Map from '../../screens/Map/Map';
 import Orders from '../../screens/Orders/Orders';
@@ -25,7 +26,7 @@ function CoreNavigation() {
 
 function CoreLayout() {
   return (
-    <View flex={1} testID="core-layout">
+    <View style={{ paddingTop: 35 }} flex={1} testID="core-layout">
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
