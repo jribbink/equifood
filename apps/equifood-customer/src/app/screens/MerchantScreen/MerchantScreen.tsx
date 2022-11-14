9c2de2f8a1bd9d6064f550830de6ef31f7b42a33
jcoll059 <jordancolledge059@gmail.com>
2022-11-14 00:03:25 -0800

    Fixed frontend merchant interface

--- apps/equifood-customer/src/app/screens/MerchantScreen/MerchantScreen.tsx
+++ apps/equifood-customer/src/app/screens/MerchantScreen/MerchantScreen.tsx
@@ -4,7 +4,7 @@ import { Merchant } from '@equifood/api-interfaces';
 import { CoreStackParams } from '../../layouts/CoreLayout/CoreNavigatorParams';
 import React, { useState } from 'react';
 import { useMerchant } from '../../hooks/useMerchant';
-import MerchantCard from '../../components/cards/MerchantCard/MerchantCard';
+import ItemCard from '../../components/cards/ItemCard/ItemCard';
 
 export interface MerchantScreenParams {
   merchant: Merchant;
@@ -15,25 +15,24 @@ function RestaurantScreen({
   route,
 }: StackScreenProps<CoreStackParams, 'merchant'>) {
   const { merchant } = useMerchant(route.params.merchant.id);
-  //const { items } = merchant.items;
-  // the above should work, but we get a Merchant from /libs/api-interfaces/src/lib/merchant.ts
-  // rather than a merchant.entity from /equifood-api/src/app/merchant/entities/merchant-entity.ts
-  // pushing what I have for the night and going to bed.
-  // also fix ItemCard as it is just a copy of MerchantCard at the moment
+  const { items } = merchant.items;
+  // why can this be undefined? fix later I guess
+
   return (
     <ScrollView testID="merchants" flex={1}>
       {
-        // fix this when you figure out how to get items working
-        /*(items || []).map((i) => (
-        <Box key={i.id} shadow="2">
-          <MerchantCard
-            merchant={i}
-            onPress={() => {
-              console.log('press');
-            }} // temporary until I get smart and figure out how to add this to the cart
-          ></MerchantCard>
-        </Box>
-          ))*/
+        // ItemCard is currently (nov 13/2022) a placeholder copied from MerchantScreen
+        // please fix
+        (items || []).map((i) => (
+          <Box key={i.id} shadow="2">
+            <ItemCard
+              merchant={i}
+              onPress={() => {
+                console.log('press');
+              }} // temporary until I get smart and figure out how to add this to the cart
+            ></ItemCard>
+          </Box>
+        ))
       }
       <Box>
         <Text>
