dc99d538b2a912c045aaff3c3db6dfe2b307ccca
jcoll059 <jordancolledge059@gmail.com>
2022-11-23 10:53:47 -0800

    Added components to MerchantScreen

--- apps/equifood-customer/src/app/screens/MerchantScreen/MerchantScreen.tsx
+++ apps/equifood-customer/src/app/screens/MerchantScreen/MerchantScreen.tsx
@@ -1,15 +1,20 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import { Box, Button, Text, ScrollView, VStack } from 'native-base';
+import {
+  Box,
+  Button,
+  Text,
+  ScrollView,
+  VStack,
+  Image,
+  View,
+  Heading,
+  HStack,
+} from 'native-base';
+import { TouchableHighlight } from 'react-native';
 import { Merchant } from '@equifood/api-interfaces';
 import { CoreStackParams } from '../../layouts/CoreLayout/CoreNavigatorParams';
 import React, { useState } from 'react';
 import { useMerchant } from '../../hooks/useMerchant';
-import ItemCard from '../../components/cards/ItemCard/ItemCard';
-import {
-  addItem,
-  removeItem,
-  setMerchant,
-} from '../../redux/slices/cart-slice';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../redux/store';
 
@@ -29,26 +34,52 @@ function RestaurantScreen({
   const item = merchant.item;
 
   return (
-    <ScrollView testID="items" flex={1}>
-      {
-        // ItemCard is currently (nov 17/2022) mostly copied from MerchantCard
-        // please fix
-        <ItemCard
-          item={item}
-          onPress={() => {
-            dispatch(addItem(item));
-            // change cart merchant here
+    <ScrollView testID="view" flex={1}>
+      <Box h="200">
+        <Image
+          width="100%"
+          height="200"
+          source={{
+            uri: merchant.banner_url,
           }}
-        ></ItemCard>
-      }
-      <Box>
-        <Text>
-          {route.params.merchant.name + ': ' + route.params.merchant.inventory}
-        </Text>
-        <Button onPress={() => navigation.navigate('core', { screen: 'home' })}>
-          Back to Home
-        </Button>
+          alt={merchant.name}
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          borderTopRadius="5"
+        />
+        <Box flex={1} justifyContent="flex-end" p="1.5">
+          <Image
+            source={{ uri: merchant.logo_url }}
+            alt={merchant.name}
+            backgroundColor="white"
+            borderRadius="full"
+            width="16"
+            height="16"
+          ></Image>
+        </Box>
       </Box>
+      <VStack space="4" m="4">
+        <Box borderRadius="5" testID="desc" shadow="2">
+          <HStack
+            bgColor="white"
+            borderBottomRadius={5}
+            shadow="5"
+            p="1.5"
+            space="2"
+          >
+            <Text>
+              <Heading testID="merchant-name" fontSize="lg" fontWeight="bold">
+                {merchant.name}
+              </Heading>
+              <Text>{'\n' + merchant.description}</Text>
+            </Text>
+          </HStack>
+        </Box>
+      </VStack>
+      <Button onPress={() => alert('checkout')}>hello</Button>
     </ScrollView>
   );
 }
