ae7eb7c715cd7005937292a10afbdfcee5131a88
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-11-24 23:53:29 -0800

    The hooks are working, The account screen looks better.
    title.

--- apps/equifood-customer/src/app/hooks/useProfile.ts
+++ apps/equifood-customer/src/app/hooks/useProfile.ts
@@ -2,11 +2,11 @@ import { User } from '@equifood/api-interfaces';
 import { useFetcher } from './useFetcher';
 
 export function useProfile() {
-    const { data, error } = useFetcher<User>(`/users/self/profile`);
-  
-    return {
-      user: data,
-      isLoading: !error && !data,
-      isError: error,
-    };
-  }
+  const { data, error } = useFetcher<User>(`/users/self/profile`);
+
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
---
+++ libs/api-interfaces/src/lib/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: string;
+  first_name: 'John';
+  last_name: 'Doe';
+  email: 'lol';
+  phone: '911';
+}
+//export interface User {
+//   id: string;
+//   first_name: string;
+//   last_name: string;
+//   email: string;
+//   phone: string;
+// }
