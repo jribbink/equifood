f92026ea22c6b3cffd7b0786428e97e17a5f088f
RileyComer <52143187+RileyComer@users.noreply.github.com>
2022-11-25 23:42:15 -0700

    Improved map (#137)
    * Improved map
    
    map now uses userLocation and removed print statement. Currently has a temporary custom image hosted on imgur. I looked for expo vector icons but could not find any URI being provided. Image is very easy to change if needed.
    
    * Formatting
    
    * remove print again
    
    * adding the icon back into branch after the merge
    
    * npx prettier --write . again
    
    * Fixed the merge stuff
    
    Co-authored-by: Jordan Colledge <71358703+ItsMyFuneral@users.noreply.github.com>

--- apps/equifood-customer/src/app/screens/Map/Map.tsx
+++ apps/equifood-customer/src/app/screens/Map/Map.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDisclose, Text, Box } from 'native-base';
+import { useDisclose, Box } from 'native-base';
 import { Merchant, Location } from '@equifood/api-interfaces';
 import { CoreNavigationProps } from '../../layouts/CoreLayout/CoreNavigatorParams';
 import MerchantCard from '../../components/cards/MerchantCard/MerchantCard';
@@ -7,26 +7,28 @@ import { useState } from 'react';
 import { useMerchants } from '../../hooks/useMerchants';
 import * as expoLocation from 'expo-location';
 import ActionSheet from '../../components/ActionSheet/ActionSheet';
-import MerchantMap from '../../components/MerchantMap/MerchantMap';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
 const Map = ({ navigation }: CoreNavigationProps<'map'>) => {
   const { isOpen, onOpen, onClose } = useDisclose();
 
   const { merchants } = useMerchants();
 
-  const [userLocation, setUserLocation] = useState<Location>({
-    address: '',
-    latitude: 49.9,
-    longitude: -119.5,
-  });
+  const [userLocation, setUserLocation] = useState<Location>();
 
   useEffect(() => {
     (async () => {
       const { status } = await expoLocation.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
+        const out: Location = {
+          address: '',
+          latitude: 49.941,
+          longitude: -119.386,
+        };
+        setUserLocation(out);
         return;
       }
-
       const location = await expoLocation.getCurrentPositionAsync({});
       const out: Location = {
         address: '',
@@ -36,6 +38,10 @@ const Map = ({ navigation }: CoreNavigationProps<'map'>) => {
       setUserLocation(out);
     })();
   }, []);
+
+  function onMerchantPress(merchant: Merchant) {
+    navigation.navigate('merchant', { merchant });
+  }
 
   const [selectedMerchant, setSelectedMerchant] = useState<Merchant | null>(
     null
@@ -49,16 +55,41 @@ const Map = ({ navigation }: CoreNavigationProps<'map'>) => {
 
   return (
     <Box height="full">
-      <MerchantMap
-        merchants={merchants}
-        initialRegion={{
-          latitude: userLocation.latitude,
-          longitude: userLocation.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        onMerchantChange={handleMerchantChange}
-      ></MerchantMap>
+      {userLocation != null && (
+        <MapView
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+          initialRegion={{
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onPress={() => {
+            onClose();
+            setSelectedMerchant(null);
+          }}
+        >
+          {(merchants || []).map((merchant) => (
+            <Marker
+              key={merchant.id}
+              coordinate={{
+                latitude: merchant.location.latitude,
+                longitude: merchant.location.longitude,
+              }}
+              title={merchant.name}
+              description={merchant.description}
+              image={{ uri: 'https://imgur.com/L5PXC8v.png' }}
+              onPress={(e) => {
+                e.stopPropagation();
+                handleMerchantChange(merchant);
+              }}
+            />
+          ))}
+        </MapView>
+      )}
       {selectedMerchant !== null && (
         <Box
           justifyContent="flex-end"
@@ -71,7 +102,6 @@ const Map = ({ navigation }: CoreNavigationProps<'map'>) => {
           pointerEvents="box-none"
         >
           <ActionSheet isOpen={isOpen} onClose={onClose}>
-            <Text>Hello</Text>
             <MerchantCard
               merchant={selectedMerchant}
               onPress={() => onMerchantPress(selectedMerchant)}
