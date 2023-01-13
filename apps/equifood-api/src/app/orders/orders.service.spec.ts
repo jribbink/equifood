20f4bcf2a3ff4aada8ba3215b05e95e148f7def9
Jordan Ribbink <jribbink@telus.net>
2023-01-12 23:22:25 -0800

    fixed

--- apps/equifood-api/src/app/orders/orders.service.spec.ts
+++ apps/equifood-api/src/app/orders/orders.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmSqlLiteTestingModule } from '../../test-utils/typeorm-test.module';
 import { Item } from '../merchant/entities/item.entity';
 import { Merchant } from '../merchant/entities/merchant.entity';
-import { User } from '../users/entities/user.entity';
+import { UsersModule } from '../users/users.module';
 import { Order } from './entities/order.entity';
 import { OrderedItem } from './entities/ordered-item.entity';
 import { OrdersService } from './orders.service';
@@ -14,7 +14,8 @@ describe('OrdersService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [OrdersService],
       imports: [
-        TypeOrmSqlLiteTestingModule([Item, OrderedItem, Merchant, Order, User]),
+        TypeOrmSqlLiteTestingModule([Item, OrderedItem, Merchant, Order]),
+        UsersModule,
       ],
     }).compile();
 
