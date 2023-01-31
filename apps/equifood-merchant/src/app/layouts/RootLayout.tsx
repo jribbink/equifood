9761e9c1ba04e9fc711af310d5cc58db7fad0caa
Jordan Ribbink <jribbink@telus.net>
2023-01-31 04:39:22 -0800

    Add useEffect hook to navigate based on login

--- apps/equifood-merchant/src/app/layouts/RootLayout.tsx
+++ apps/equifood-merchant/src/app/layouts/RootLayout.tsx
@@ -2,13 +2,20 @@ import {
   NavigationContainer,
   createNavigationContainerRef,
   NavigatorScreenParams,
+  CompositeScreenProps,
 } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  StackScreenProps,
+} from '@react-navigation/stack';
 import { View } from 'native-base';
 //import CartButton from '../../components/buttons/CartButton/CartButton';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import CoreLayout, { CoreNavigationParams } from './CoreLayout';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import { useEffect } from 'react';
+import { useAuth } from '@equifood/ui-shared';
 
 const Stack = createStackNavigator<RootNavigationParams>();
 
@@ -17,14 +24,22 @@ export type RootNavigationParams = {
   login: undefined;
 };
 
-/*export type RootNavigationProps<T extends keyof RootNavigationParams> =
+export type RootNavigationProps<T extends keyof CoreNavigationParams> =
   CompositeScreenProps<
-    BottomTabScreenProps<CoreTabParams, T>,
-    StackScreenProps<CoreStackParams, 'core'>
-  >;*/
+    BottomTabScreenProps<CoreNavigationParams, T>,
+    StackScreenProps<RootNavigationParams, 'core'>
+  >;
 
 function RootLayout() {
   const navigation = createNavigationContainerRef<RootNavigationParams>();
+  const { token } = useAuth();
+
+  console.log(token);
+
+  useEffect(() => {
+    if (token) navigation.navigate('core', { screen: 'home' });
+    else navigation.navigate('login');
+  }, [token, navigation]);
 
   return (
     <View flex={1} testID="core-layout">
