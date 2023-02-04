233a73362d2c4b19db5f42ad3125742030a98f91
RileyComer <2001rdc@gmail.com>
2023-02-03 17:49:54 -0800

    Simple Distance Formula

--- libs/ui-shared/src/components/molecules/cards/MerchantCard/MerchantCard.tsx
+++ libs/ui-shared/src/components/molecules/cards/MerchantCard/MerchantCard.tsx
@@ -1,6 +1,7 @@
-import { Box, Heading, HStack, Image } from 'native-base';
+import { Box, Heading, HStack, Image, Text } from 'native-base';
 import { Merchant } from '@equifood/api-interfaces';
 import { GestureResponderEvent, TouchableHighlight } from 'react-native';
+import { useLocation } from 'libs/ui-shared/src/hooks';
 
 interface Props {
   merchant: Merchant;
@@ -8,6 +9,32 @@ interface Props {
 }
 
 export const MerchantCard = ({ merchant, onPress }: Props) => {
+
+  function getDistanceFromLatLonInKm(lat1:number, lon1:number, lat2:number, lon2:number) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  function deg2rad(deg :number) {
+    return deg * (Math.PI/180)
+  }
+
+  const userLocation=useLocation();
+
+  var distance=0;
+  if(userLocation){
+    distance=Math.round(getDistanceFromLatLonInKm(merchant.location.latitude, merchant.location.longitude, userLocation.latitude, userLocation.longitude));
+  }
+
   return (
     <TouchableHighlight
       onPress={onPress}
@@ -50,6 +77,7 @@ export const MerchantCard = ({ merchant, onPress }: Props) => {
           <Heading testID="merchant-name" fontSize="md" fontWeight="bold">
             {merchant.name}
           </Heading>
+          <Text>{distance} km</Text>
         </HStack>
       </Box>
     </TouchableHighlight>
