3d37d8b1f8739c7929dc188ccb3c82e1d860bd55
Joonsik Kim <60025350+prokjs@users.noreply.github.com>
2023-02-03 22:49:22 -0800

    Improved merchant page appearance (#226)

--- libs/ui-shared/src/components/molecules/cards/ItemCard/ItemCard.spec.tsx
+++ libs/ui-shared/src/components/molecules/cards/ItemCard/ItemCard.spec.tsx
@@ -4,15 +4,23 @@ import { render } from '../../../../test-utils/render';
 import { ItemCard } from './ItemCard';
 import { Item } from '@equifood/api-interfaces';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {};
+
 describe('MerchantCard tests', () => {
   test('renders name correctly', async () => {
     const item: Item = {
       id: '1234',
       name: 'foo',
-      oldPrice: 6.99,
-      newPrice: 5.99,
+      originalPrice: 6.99,
+      price: 5.99,
+      description: 'Item description',
+      allergies: '',
+      quantity: 1,
     };
-    const { getByTestId } = await render(<ItemCard item={item} />);
+    const { getByTestId } = await render(
+      <ItemCard item={item} quantity={2} onQuantityChange={noop} />
+    );
     expect(getByTestId('item-name')).toHaveTextContent(item.name);
   });
 });
