e3a3b188dbc742bea7a5e62b2c7044324f095020
jcoll059 <jordancolledge059@gmail.com>
2023-02-17 12:30:02 -0800

    Merchant side OrderList displays something

--- apps/equifood-merchant/src/app/layouts/CoreLayout.tsx
+++ apps/equifood-merchant/src/app/layouts/CoreLayout.tsx
@@ -2,11 +2,13 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AccountScreen from '../screens/AccountScreen/AccountScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import MenuScreen from '../screens/MenuScreen/MenuScreen';
+import OrderListScreen from '../screens/OrderListScreen/OrderListScreen';
 
 export type CoreNavigationParams = {
   home: undefined;
   menu: undefined;
   account: undefined;
+  orders: undefined;
 };
 
 const Tab = createBottomTabNavigator<CoreNavigationParams>();
@@ -16,6 +18,7 @@ function CoreLayout() {
     <Tab.Navigator>
       <Tab.Screen name="home" component={HomeScreen}></Tab.Screen>
       <Tab.Screen name="menu" component={MenuScreen}></Tab.Screen>
+      <Tab.Screen name="orders" component={OrderListScreen}></Tab.Screen>
       <Tab.Screen name="account" component={AccountScreen}></Tab.Screen>
     </Tab.Navigator>
   );
