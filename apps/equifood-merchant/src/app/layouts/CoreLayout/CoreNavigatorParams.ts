35f3b1d95eea3d5c99b5ca0e1a6d980015049d47
jcoll059 <jordancolledge059@gmail.com>
2023-02-17 10:12:10 -0800

    Created basic CoreLayout info for merchant side

---
+++ apps/equifood-merchant/src/app/layouts/CoreLayout/CoreNavigatorParams.ts
@@ -0,0 +1,37 @@
+import { Merchant, Order, Item } from '@equifood/api-interfaces';
+import type {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
+import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import type { StackScreenProps } from '@react-navigation/stack';
+
+export type CoreTabParams = {
+  home: undefined;
+  map: undefined;
+  orders: undefined;
+  account: undefined;
+  orderConfirm: undefined;
+};
+
+export type CoreStackParams = {
+  core: NavigatorScreenParams<CoreTabParams>;
+  merchant: {
+    merchant: Merchant;
+  };
+  order: {
+    order: Order;
+  };
+  cart: undefined;
+  orderConfirm: {
+    merchant: Merchant;
+    items: Item[];
+    quantities: { [id: string]: number };
+  };
+};
+
+export type CoreNavigationProps<T extends keyof CoreTabParams> =
+  CompositeScreenProps<
+    BottomTabScreenProps<CoreTabParams, T>,
+    StackScreenProps<CoreStackParams, 'core'>
+  >;
---
+++ apps/equifood-merchant/src/app/screens/OrderListScreen/OrderListScreen.tsx
@@ -0,0 +1,5 @@
+function OrderListScreen() {
+  return null;
+}
+
+export default OrderListScreen;
