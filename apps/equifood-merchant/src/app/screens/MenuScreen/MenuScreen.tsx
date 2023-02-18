14b40b9285c1d7b8073efa0a51246ce9fab94d67
Jordan Ribbink <jribbink@telus.net>
2023-02-17 19:03:10 -0800

    stash

--- apps/equifood-merchant/src/app/screens/MenuScreen/MenuScreen.tsx
+++ apps/equifood-merchant/src/app/screens/MenuScreen/MenuScreen.tsx
@@ -3,34 +3,30 @@ import { ItemCard, NewItemButton, useMerchant } from '@equifood/ui-shared';
 import { HStack, VStack } from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import ItemEditorModal from '../ItemEditorScreen/ItemEditorScreen';
+import { CoreNavigationProps } from '../../layouts/CoreLayout';
 
-function MenuScreen() {
+function MenuScreen({ navigation }: CoreNavigationProps<'menu'>) {
   const { merchant } = useMerchant('self');
   const [editItem, setEditItem] = useState<Item | null>(null);
 
   function handleItemPress(item: Item) {
-    console.log(item);
+    navigation.navigate('itemEditor', { item });
   }
 
   return (
-    <>
-      <VStack space="5">
-        {(merchant?.items || []).map((item) => (
-          <TouchableOpacity key={item.id} onPress={() => handleItemPress(item)}>
-            <ItemCard key={item.id} item={item}></ItemCard>
-          </TouchableOpacity>
-        ))}
+    <VStack space="5">
+      {(merchant?.items || []).map((item) => (
+        <TouchableOpacity key={item.id} onPress={() => handleItemPress(item)}>
+          <ItemCard key={item.id} item={item}></ItemCard>
+        </TouchableOpacity>
+      ))}
 
-        <HStack justifyContent="center">
-          <NewItemButton onPress={() => setEditItem({})}></NewItemButton>
-        </HStack>
-      </VStack>
-      <ItemEditorModal
-        isOpen={!!editItem}
-        onClose={() => setEditItem(null)}
-      ></ItemEditorModal>
-    </>
+      <HStack justifyContent="center">
+        <NewItemButton
+          onPress={() => navigation.navigate('itemEditor')}
+        ></NewItemButton>
+      </HStack>
+    </VStack>
   );
 }
 
