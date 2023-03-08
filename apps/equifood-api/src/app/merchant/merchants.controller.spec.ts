48d733008ecb4f1820b0120935778d02d16bd06d
Jordan Ribbink <jribbink@telus.net>
2023-03-08 00:19:44 -0800

    fix tests

--- apps/equifood-api/src/app/merchant/merchants.controller.spec.ts
+++ apps/equifood-api/src/app/merchant/merchants.controller.spec.ts
@@ -5,13 +5,18 @@ import { MerchantsService } from './merchants.service';
 import { TypeOrmSqlLiteTestingModule } from '../../test-utils/typeorm-test.module';
 import { Item } from './items/entities/item.entity';
 import { OrdersModule } from '../orders/orders.module';
+import { UsersModule } from '../users/users.module';
 
 describe('MerchantsController', () => {
   let controller: MerchantsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmSqlLiteTestingModule([Merchant, Item]), OrdersModule],
+      imports: [
+        TypeOrmSqlLiteTestingModule([Merchant, Item]),
+        OrdersModule,
+        UsersModule,
+      ],
       controllers: [MerchantsController],
       providers: [MerchantsService],
     }).compile();
