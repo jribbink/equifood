ef117573508a45643ecf32e1a4feea96a592ee02
Jordan Ribbink <jribbink@telus.net>
2023-03-14 16:46:57 -0700

    Calculate total price for item in checkout (quantity x price) instead of price for one item

--- libs/ui-shared/src/components/molecules/cards/CheckoutItemCard/CheckoutItemCard.tsx
+++ libs/ui-shared/src/components/molecules/cards/CheckoutItemCard/CheckoutItemCard.tsx
@@ -29,7 +29,7 @@ export const CheckoutItemCard = ({ item, quantity }: Props) => {
           color={'primary.600'}
           style={{ textDecorationLine: 'line-through' }}
         >
-          {'$ ' + item.originalPrice.toFixed(2)}
+          {'$ ' + (item.originalPrice * quantity).toFixed(2)}
         </Text>
         <Text
           textAlign="right"
@@ -39,7 +39,7 @@ export const CheckoutItemCard = ({ item, quantity }: Props) => {
           fontSize="20"
           minWidth="20"
         >
-          {'$ ' + item.price.toFixed(2)}
+          {'$ ' + (item.price * quantity).toFixed(2)}
         </Text>
       </HStack>
     </Box>
