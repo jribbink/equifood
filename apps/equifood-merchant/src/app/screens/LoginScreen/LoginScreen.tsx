0d75dba71dc373634e2914ba42adaae7bbd6b7b7
jcoll059 <jordancolledge059@gmail.com>
2023-03-17 18:33:39 -0700

    Merchant login screen better

--- apps/equifood-merchant/src/app/screens/LoginScreen/LoginScreen.tsx
+++ apps/equifood-merchant/src/app/screens/LoginScreen/LoginScreen.tsx
@@ -1,12 +1,29 @@
-import { LoginView } from '@equifood/ui-shared';
-import { Box } from 'native-base';
+import React from 'react';
 
-function LoginScreen() {
+import { Box, HStack, VStack } from 'native-base';
+import { Text, TouchableOpacity } from 'react-native';
+import { LoginView } from '@equifood/ui-shared';
+import { equifoodTheme } from '@equifood/ui-shared';
+
+const LoginScreen = ({ navigation }) => {
   return (
-    <Box>
+    <Box flex={1} testID="login-screen">
       <LoginView allowedRoles={['merchant']}></LoginView>
+
+      <VStack flexDirection="column" p="3" space="3">
+        <Box>
+          <HStack>
+            <Text style={{ fontSize: 15 }}>Don't have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('signup')}>
+              <Text style={{ color: equifoodTheme.colors.primary[500] }}>
+                Sign up
+              </Text>
+            </TouchableOpacity>
+          </HStack>
+        </Box>
+      </VStack>
     </Box>
   );
-}
+};
 
 export default LoginScreen;
