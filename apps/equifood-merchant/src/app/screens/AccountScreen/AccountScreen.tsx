c80d5b7a2224ed805b2fcd6c0e1e87ec29ff8aa2
RileyComer <2001rdc@gmail.com>
2023-04-04 15:58:13 -0700

    Styled update merchant info page.
    Doesn't update in real-time at moment.

--- apps/equifood-merchant/src/app/screens/AccountScreen/AccountScreen.tsx
+++ apps/equifood-merchant/src/app/screens/AccountScreen/AccountScreen.tsx
@@ -1,5 +1,5 @@
 import { useAuth, useAxios, useMerchant } from '@equifood/ui-shared';
-import { Button, VStack, Text } from 'native-base';
+import { Button, VStack, Text, Box } from 'native-base';
 import { useEffect, useState } from 'react';
 import { StyleSheet, TextInput } from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
@@ -13,8 +13,6 @@ function AccountScreen() {
   const [description, setDescription] = useState('');
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
-
-  const [logo, setLogo] = useState('');
 
   const styles = StyleSheet.create({
     input: {
@@ -36,33 +34,7 @@ function AccountScreen() {
     setAddress(""+merchant?.location.address);
   }, [merchant]);
 
-  const options = {
-    title: 'Select Image',
-    customButtons: [
-      { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
-    ],
-    storageOptions: {
-      skipBackup: true,
-      path: 'images',
-    },
-  };
-
-  async function changeLogo(){
-    const url=await axios.post('/uploads/'+/*imageID*/+'/'+/* imageName*/+'/update',
-    {id:merchant?.id});
-    await axios.post('/merchants/$'+merchant?.id+'/update',
-    {
-      id: merchant?.id,
-      name: merchant?.name,
-      banner: merchant?.banner_url,
-      logo: url,
-      description: merchant?.description,
-      phone_number: merchant?.phone_number,
-      location: merchant?.location
-    });
-  }
-
-  async function changeMerchant() {
+  async function updateMerchant() {
     await axios.post('/merchants/$'+merchant?.id+'/update',
     {
       id: merchant?.id,
@@ -81,53 +53,60 @@ function AccountScreen() {
 
   return (
     <VStack>
+      <Box padding={5}>
+        <Text paddingLeft={5}>Name:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setName}
+          placeholder="Change merchant name?"
+          value={name}
+          testID="nameInput"
+          autoCapitalize="none"
+          placeholderTextColor={'yellowgreen'}
+        />
 
-      <Text>Name</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setName}
-        placeholder="Change merchant name?"
-        value={name}
-        testID="nameInput"
-        autoCapitalize="none"
-        placeholderTextColor={'yellowgreen'}
-      />
+        <Text paddingLeft={5}>Description:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setDescription}
+          placeholder="Change merchant description?"
+          value={description}
+          testID="descriptionInput"
+          autoCapitalize="none"
+          placeholderTextColor={'yellowgreen'}
+        />
 
-      <Text>Description</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setDescription}
-        placeholder="Change merchant description?"
-        value={description}
-        testID="descriptionInput"
-        autoCapitalize="none"
-        placeholderTextColor={'yellowgreen'}
-      />
+        <Text paddingLeft={5}>Phone:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPhone}
+          placeholder="Change merchant phone number?"
+          value={phone}
+          testID="phoneInput"
+          autoCapitalize="none"
+          placeholderTextColor={'yellowgreen'}
+        />
 
-      <Text>phone</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setPhone}
-        placeholder="Change merchant phone number?"
-        value={phone}
-        testID="phoneInput"
-        autoCapitalize="none"
-        placeholderTextColor={'yellowgreen'}
-      />
+        <Text paddingLeft={5}>Address:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setAddress}
+          placeholder="Change address?"
+          value={address}
+          testID="addressInput"
+          autoCapitalize="none"
+          placeholderTextColor={'yellowgreen'}
+        />
+      </Box>
+      <Button
+      backgroundColor={'green.900'}
+      margin={10}
+      onPress={() => updateMerchant()}>Update information</Button>
 
-      <Text>Address</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setAddress}
-        placeholder="Change address?"
-        value={address}
-        testID="addressInput"
-        autoCapitalize="none"
-        placeholderTextColor={'yellowgreen'}
-      />
-      <Button onPress={() => changeMerchant()}>Change user information</Button>
-
-      <Button onPress={() => setJwt(null)}>Logout</Button>
+      <Button
+      margin={5}
+      marginTop={100}
+      onPress={() => setJwt(null)}>Logout</Button>
     </VStack>
   );
 }
